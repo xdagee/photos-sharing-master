d76415344e3328cc22db669ea8943886af4374db
Prince Gee <14103448+xdagee@users.noreply.github.com>
2019-06-19 11:17:04 +0000

    Initial commit

---
+++ ios/Runner/GeneratedPluginRegistrant.m
@@ -0,0 +1,22 @@
+//
+//  Generated file. Do not edit.
+//
+
+#import "GeneratedPluginRegistrant.h"
+#import <fluttertoast/FluttertoastPlugin.h>
+#import <google_sign_in/GoogleSignInPlugin.h>
+#import <image_picker/ImagePickerPlugin.h>
+#import <path_provider/PathProviderPlugin.h>
+#import <sqflite/SqflitePlugin.h>
+
+@implementation GeneratedPluginRegistrant
+
++ (void)registerWithRegistry:(NSObject<FlutterPluginRegistry>*)registry {
+  [FluttertoastPlugin registerWithRegistrar:[registry registrarForPlugin:@"FluttertoastPlugin"]];
+  [FLTGoogleSignInPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTGoogleSignInPlugin"]];
+  [FLTImagePickerPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTImagePickerPlugin"]];
+  [FLTPathProviderPlugin registerWithRegistrar:[registry registrarForPlugin:@"FLTPathProviderPlugin"]];
+  [SqflitePlugin registerWithRegistrar:[registry registrarForPlugin:@"SqflitePlugin"]];
+}
+
+@end
---
+++ lib/pages/join_trip_page.dart
@@ -0,0 +1,102 @@
+/*
+ * Copyright 2019 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import 'package:flutter/material.dart';
+import 'package:scoped_model/scoped_model.dart';
+import 'package:sharing_codelab/components/primary_raised_button.dart';
+import 'package:sharing_codelab/model/photos_library_api_model.dart';
+import 'package:sharing_codelab/photos_library_api/album.dart';
+import 'package:sharing_codelab/photos_library_api/join_shared_album_response.dart';
+import 'package:sharing_codelab/photos_library_api/share_album_response.dart';
+import 'package:sharing_codelab/util/to_be_implemented.dart';
+
+class JoinTripPage extends StatefulWidget {
+  @override
+  _JoinTripPageState createState() => _JoinTripPageState();
+}
+
+class _JoinTripPageState extends State<JoinTripPage> {
+  final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+  bool _isLoading = false;
+  final TextEditingController shareTokenFormController =
+      TextEditingController();
+
+  @override
+  void dispose() {
+    shareTokenFormController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(),
+      body: Container(
+        padding: const EdgeInsets.all(25),
+        child: _isLoading
+            ? Center(child: const CircularProgressIndicator())
+            : Form(
+                key: _formKey,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    TextFormField(
+                      controller: shareTokenFormController,
+                      autocorrect: true,
+                      decoration: InputDecoration(
+                        hintText: 'Paste the share token',
+                      ),
+                    ),
+                    Container(
+                      padding: const EdgeInsets.symmetric(
+                        vertical: 15,
+                        horizontal: 0,
+                      ),
+                      child: const Text(
+                        'This will join an album in your Google Photos account',
+                        style: TextStyle(
+                          color: Colors.grey,
+                        ),
+                      ),
+                    ),
+                    Center(
+                      child: RaisedButton(
+                        onPressed: () => _joinTrip(context),
+                        child: const Text('Join Trip'),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+      ),
+    );
+  }
+
+  Future<void> _joinTrip(BuildContext context) async {
+    // TODO(codelab): Implement this call
+    ToBeImplemented.showMessage();
+
+    // Show loading indicator
+
+    // Call the API to join an album with the entered share token
+
+    // Hide loading indicator
+
+    // Return to the previous screen
+    Navigator.pop(context);
+  }
+}
